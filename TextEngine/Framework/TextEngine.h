cca946535bfe2c1141e9b15e1501d89072fa200b
konh <konh@yandex.ru>
2018-05-04 19:14:07 +0700

    Shared library: compilable lib for windows & android;

--- TextEngine/Framework/TextEngine.h
+++ TextEngine/Framework/TextEngine.h
@@ -2,19 +2,26 @@
 
 #include <string>
 
+#if defined (_MSC_VER )
+	#define MS_DLL_EXPORT _declspec(dllexport)
+#else
+	#define MS_DLL_EXPORT
+#endif
+
+
 typedef void(*WriteCallback)(const char*);
 typedef void(*DebugCallback)(const char*);
 
 extern "C" {
 	
 	// Called from frontend code to initialize all required callbacks
-	void _declspec(dllexport) TextEngine_Init(WriteCallback writeCallback, DebugCallback debugCallback);
+	void MS_DLL_EXPORT TextEngine_Init(WriteCallback writeCallback, DebugCallback debugCallback);
 	
 	// Called from frontend code when it is ready
-	void _declspec(dllexport) TextEngine_OnStart();
+	void MS_DLL_EXPORT TextEngine_OnStart();
 	
 	// Called from frontend code on user input
-	void _declspec(dllexport) TextEngine_OnRead(const char* msg);
+	void MS_DLL_EXPORT TextEngine_OnRead(const char* msg);
 }
 
 // Called by user library, forwarded to frontend
