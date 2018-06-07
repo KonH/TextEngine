76a4799c93c47e96a2f457e9e1ab55d2edf1e65c
konh <konh@yandex.ru>
2018-06-07 18:46:42 +0700

    Toolchain: copy build result and strip sources;

--- EngineBuilder/Commands/Web/BuildCommand.cs
+++ EngineBuilder/Commands/Web/BuildCommand.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Collections.Generic;
 using EngineBuilder.Tools;
 
 namespace EngineBuilder.Commands.Web {
@@ -13,15 +14,24 @@ namespace EngineBuilder.Commands.Web {
 		public override void Run(Configuration config, CommandArguments args) {
 			base.Run(config, args);
 
-			// Copy to builds later
-			//var target      = args.GetTarget(this);
-			//var outputPath  = Path.Combine(Directory.GetCurrentDirectory(), config.BuildsDirectory, target);
-
 			var workDir = Path.Combine(Directory.GetCurrentDirectory(), config.StagingDirectory, Configuration.WebTarget);
 			var buildArgs = string.Join(" ", GetFilesToBuild(workDir));
 			buildArgs += " -s EXPORTED_FUNCTIONS=[" + string.Join(",", config.Web.ExportedFuncs.Select(s => $"\"_{s}\"")) + "]" + " -s ASSERTIONS=2";
 			Console.WriteLine($"Start Emscripten build with args: '{buildArgs}'");
 			ProcessTools.RunProcessAndEnsureSuccess(this, "Emscripten Build", config.Web.EmccPath, buildArgs, workDir);
+
+			var outputPath = Path.Combine(config.BuildsDirectory, Configuration.WebTarget);
+			Console.WriteLine($"Copy build to '{outputPath}'");
+			IOTools.CopyDirectory(workDir, outputPath);
+
+			Console.WriteLine($"Strip sources from '{outputPath}'");
+			var filesToStrip = new List<string>();
+			filesToStrip.AddRange(Directory.GetFiles(outputPath, "*.cpp", SearchOption.AllDirectories));
+			filesToStrip.AddRange(Directory.GetFiles(outputPath, "*.h", SearchOption.AllDirectories));
+			foreach ( var file in filesToStrip ) {
+				Console.WriteLine($"Stripping '{file}'");
+				File.Delete(file);
+			}
 		}
 	}
 }
